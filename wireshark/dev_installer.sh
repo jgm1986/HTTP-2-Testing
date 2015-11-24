ebc34609e6afee58fd06889e40a5b9b443bcef11
Javier Gusano Martínez <jgm1986@users.noreply.github.com>
2015-11-24 11:12:40 +0100

    Final wireshark instalaltion script.

--- wireshark/dev_installer.sh
+++ wireshark/dev_installer.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 set -e
-echo "Warning! This scripts install the develop version of Wireshark on your system."
+echo "Warning! This scripts installs the develop version of Wireshark on your system (last build)."
 
-# Clonning Wireshark repository
-git clone  https://code.wireshark.org/review/p/wireshark.git
+# Installing dependencies.
+sudo apt-get install qt-sdk curl libgtk-3-dev libpcap-dev -y
 
-cd wireshark
+# Downloading lastest build Wireshark for Linux
+URL="https://www.wireshark.org/download/automated/src/"
+FILENAME="$(curl $URL | sed -n 's/.*href="\([^"]*\).*/\1/p' | sed '7q;d')"
+DOWNLOAD=$URL$FILENAME
+wget $DOWNLOAD
+tar xfv wireshark*
+cd wireshark*
+
+# Build and install Wireshark
 ./configure
 make
 sudo make install
-
+echo "Done!"
