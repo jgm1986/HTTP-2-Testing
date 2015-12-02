b6be1c97b26105f3ca5b91e71a8d83ec288a0b20
Javier Gusano Martínez <jgm1986@users.noreply.github.com>
2015-12-02 17:32:26 +0100

    Basic installation script for Nginx server.

---
+++ server/nginx_installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+mkdir -p packages
+cd packages
+
+echo "******************************************************"
+echo "* NGINX: 1.9.5                                       *"
+echo "******************************************************"
+wget http://nginx.org/download/nginx-1.9.5.tar.gz
+tar xfv nginx-1.9.5.tar.gz
+cd nginx-1.9.5/
+./configure --without-http_rewrite_module
+make
+sudo make install
